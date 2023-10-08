d18ade6255010b627b981cd0c60f188a52637e08
belkud <vlad-belkud@yandex.ru>
2023-10-08 18:36:24 +0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
+
+
+let button = document.querySelectorAll ('.btn') 
+button.forEach, (addEventListener.btn =>
+   btn ('click', function(){
+
+  }  ))

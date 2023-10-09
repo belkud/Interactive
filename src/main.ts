4a2d075868a93204264f1791ba838d438a7b3c5a
belkud <vlad-belkud@yandex.ru>
2023-10-09 12:12:50 +0300

    uuku

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
 
-let button = document.querySelectorAll ('.btn') 
-button.forEach, (addEventListener.btn =>
-   btn ('click', function(){
+let blueButtons = document.querySelectorAll('.btn')
+blueButtons.forEach (btn=>
+  btn.addEventListener('click', function(e){
+     console.log(parseInt(e.target.dataset.num));
+     
+  }))
 
-  }  ))
+
+  let balls = document.querySelector ('.ball')
+  let fields = document.querySelector ('.field')
+
+  fields.addEventListener ('click', function(e){
+   balls.style.left = e.clientX + 'px'
+   balls.style.top = e.clientY + 'px'
+  })
